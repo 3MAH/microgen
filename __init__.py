431b4a21add2deb623699dc2dfa4a8ef5d411f2a
Yves Chemisky <yves.chemisky@gmail.com>
2021-07-12 15:35:22 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -2,6 +2,9 @@ from .Sphere import *
 from .Cylinder import *
 from .Bar import *
 from .Ellipsoid import *
+from .Material_functions import *
+from .Material_sections import *
+from .Mesh import *
 from .Functions import *
 from .Phase import *
 from .Rve import *
