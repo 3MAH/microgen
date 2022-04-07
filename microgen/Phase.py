935a0016ba924382e168c70ca9761cf8e9067f87
Kevin Marchais <kevinmarchais@gmail.com>
2022-04-07 09:55:39 +0200

    commented readPhases

--- microgen/Phase.py
+++ microgen/Phase.py
@@ -17,7 +17,7 @@ class BasicGeometry:
         self, number, shape, xc, yc, zc, psi, theta, phi, param_geom, path_data=None
     ):
         self.number = number
-        self.shape = shape.lower()
+        self.shape = shape
         self.xc = xc
         self.yc = yc
         self.zc = zc
@@ -31,7 +31,7 @@ class BasicGeometry:
         self.angle = np.array([self.psi, self.theta, self.phi])
         self.name = self.shape + str(self.number)
 
-        if self.shape == "box":
+        if self.shape.lower() == "box":
             self.geometry = Box(
                 center=self.center,
                 angle=self.angle,
@@ -40,7 +40,7 @@ class BasicGeometry:
                 a3=self.param_geom["a3"],
                 number=self.number,
             )
-        if self.shape == "cylinder":
+        if self.shape.lower() == "cylinder":
             self.geometry = Cylinder(
                 center=self.center,
                 angle=self.angle,
@@ -48,7 +48,7 @@ class BasicGeometry:
                 radius=self.param_geom["radius"],
                 number=self.number,
             )
-        if self.shape == "extrudedpolygon":
+        if self.shape.lower() == "extrudedpolygon":
             self.geometry = ExtrudedPolygon(
                 center=self.center,
                 angle=self.angle,
@@ -56,7 +56,7 @@ class BasicGeometry:
                 height=self.param_geom["height"],
                 number=self.number,
             )
-        if self.shape == "bar":
+        if self.shape.lower() == "bar":
             self.geometry = Bar(
                 center=self.center,
                 angle=self.angle,
@@ -64,13 +64,13 @@ class BasicGeometry:
                 radius=self.param_geom["radius"],
                 number=self.number,
             )
-        if self.shape == "sphere":
+        if self.shape.lower() == "sphere":
             self.geometry = Sphere(
                 center=self.center,
                 radius=self.param_geom["radius"],
                 number=self.number
             )
-        if self.shape == "ellipsoid":
+        if self.shape.lower() == "ellipsoid":
             self.geometry = Ellipsoid(
                 center=self.center,
                 angle=self.angle,
@@ -79,7 +79,7 @@ class BasicGeometry:
                 a3=self.param_geom["a3"],
                 number=self.number,
             )
-        if self.shape == "tpms":
+        if self.shape.lower() == "tpms":
             self.geometry = Tpms(
                 center=self.center,
                 angle=self.angle,
@@ -88,7 +88,7 @@ class BasicGeometry:
                 thickness=self.param_geom["thickness"],
                 number=self.number,
             )
-        if self.shape == "polyhedron":
+        if self.shape.lower() == "polyhedron":
             self.geometry = Polyhedron(
                 dic=self.param_geom,
                 number=self.number
@@ -102,21 +102,21 @@ class BasicGeometry:
 
     def generate(self, rve=None):
 
-        if self.shape == "box":
+        if self.shape.lower() == "box":
             cqshape = self.geometry.createBox()
-        elif self.shape == "cylinder":
+        elif self.shape.lower() == "cylinder":
             cqshape = self.geometry.createCylinder()
-        elif self.shape == "extrudedpolygon":
+        elif self.shape.lower() == "extrudedpolygon":
             cqshape = self.geometry.createExtrudedpolygon()
-        elif self.shape == "bar":
+        elif self.shape.lower() == "bar":
             cqshape = self.geometry.createBar()
-        elif self.shape == "sphere":
+        elif self.shape.lower() == "sphere":
             cqshape = self.geometry.createSphere()
-        elif self.shape == "ellipsoid":
+        elif self.shape.lower() == "ellipsoid":
             cqshape = self.geometry.createEllipsoid()
-        elif self.shape == "tpms":
+        elif self.shape.lower() == "tpms":
             cqshape = self.geometry.createTpms(self.path_data, rve)
-        elif self.shape == "polyhedron":
+        elif self.shape.lower() == "polyhedron":
             cqshape = self.geometry.createPolyhedron()
         else:
             raise ValueError(self.shape + " is not recognised")
@@ -124,79 +124,79 @@ class BasicGeometry:
         return cq.Shape(cqshape.val().wrapped)
 
 
-def readPhases(path_data, phases_file, phases):
+# def readPhases(path_data, phases_file, phases):
 
-    nphases = 0
-    cnt_phase = 0
-    nprops = []
-    # buf = ""
-    path_inputfile = path_data + phases_file
-    removeEmptyLines(path_inputfile)
+#     nphases = 0
+#     cnt_phase = 0
+#     nprops = []
+#     # buf = ""
+#     path_inputfile = path_data + phases_file
+#     removeEmptyLines(path_inputfile)
 
-    try:
-        fp = open(path_inputfile)
-        for line in enumerate(fp):
-            if line == "\n":
-                nphases -= 1
-            else:
-                row_split = line[1].split()
-                if nphases > 0:
+#     try:
+#         fp = open(path_inputfile)
+#         for line in enumerate(fp):
+#             if line == "\n":
+#                 nphases -= 1
+#             else:
+#                 row_split = line[1].split()
+#                 if nphases > 0:
 
-                    nprops_phase = 0
-                    if row_split[1] == "matrix":
-                        nprops_phase = 0
-                    elif row_split[1] == "sphere":
-                        nprops_phase = 1
-                    elif row_split[1] == "cylinder":
-                        nprops_phase = 2
-                    elif row_split[1] == "bar":
-                        nprops_phase = 2
-                    elif row_split[1] == "ellipsoid":
-                        nprops_phase = 3
-                    elif row_split[1] == "tpms":
-                        nprops_phase = 3
+#                     nprops_phase = 0
+#                     if row_split[1] == "matrix":
+#                         nprops_phase = 0
+#                     elif row_split[1] == "sphere":
+#                         nprops_phase = 1
+#                     elif row_split[1] == "cylinder":
+#                         nprops_phase = 2
+#                     elif row_split[1] == "bar":
+#                         nprops_phase = 2
+#                     elif row_split[1] == "ellipsoid":
+#                         nprops_phase = 3
+#                     elif row_split[1] == "tpms":
+#                         nprops_phase = 3
 
-                    nprops.append(nprops_phase)
-                nphases += 1
-    finally:
-        fp.seek(0)
-        fp.close()
+#                     nprops.append(nprops_phase)
+#                 nphases += 1
+#     finally:
+#         fp.seek(0)
+#         fp.close()
 
-    print(path_data)
-    print(phases_file)
+#     print(path_data)
+#     print(phases_file)
 
-    try:
-        fp = open(path_inputfile)
-        for line in enumerate(fp):
-            print(line)
-            if line == "\n":
-                cnt_phase -= 1
-            else:
-                row_split = line[1].split()
-                if cnt_phase > 0:
+#     try:
+#         fp = open(path_inputfile)
+#         for line in enumerate(fp):
+#             print(line)
+#             if line == "\n":
+#                 cnt_phase -= 1
+#             else:
+#                 row_split = line[1].split()
+#                 if cnt_phase > 0:
 
-                    number = int(row_split[0])
-                    shape = row_split[1]
-                    xc = float(row_split[2])
-                    yc = float(row_split[3])
-                    zc = float(row_split[4])
-                    psi = float(row_split[5])
-                    theta = float(row_split[6])
-                    phi = float(row_split[7])
+#                     number = int(row_split[0])
+#                     shape = row_split[1]
+#                     xc = float(row_split[2])
+#                     yc = float(row_split[3])
+#                     zc = float(row_split[4])
+#                     psi = float(row_split[5])
+#                     theta = float(row_split[6])
+#                     phi = float(row_split[7])
 
-                    props = []
-                    if shape == "tpms":
-                        for prop in range(0, nprops[cnt_phase - 1]):
-                            props.append(row_split[8 + prop])
-                    else:
-                        for prop in range(0, nprops[cnt_phase - 1]):
-                            props.append(float(row_split[8 + prop]))
+#                     props = []
+#                     if shape == "tpms":
+#                         for prop in range(0, nprops[cnt_phase - 1]):
+#                             props.append(row_split[8 + prop])
+#                     else:
+#                         for prop in range(0, nprops[cnt_phase - 1]):
+#                             props.append(float(row_split[8 + prop]))
 
-                    pha = phase(
-                        number, shape, xc, yc, zc, psi, theta, phi, props, path_data
-                    )
-                    phases.append(pha)
-                    print(phases[-1].shape)
-                cnt_phase += 1
-    finally:
-        fp.close()
+#                     pha = phase(
+#                         number, shape, xc, yc, zc, psi, theta, phi, props, path_data
+#                     )
+#                     phases.append(pha)
+#                     print(phases[-1].shape)
+#                 cnt_phase += 1
+#     finally:
+#         fp.close()
