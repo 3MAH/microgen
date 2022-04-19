f84738584a1796194edd78227dc4e3fb5598600e
Kevin Marchais <kevinmarchais@gmail.com>
2022-04-19 16:07:46 +0200

    customTPMS

--- examples/triplyPeriodicMinimalSurfaces/custom.py
+++ examples/triplyPeriodicMinimalSurfaces/custom.py
@@ -3,20 +3,18 @@ from microgen import Rve, BasicGeometry
 
 import argparse
 
-surface   = "custom"
 part      = "skeletal"
 thickness = 0.1
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-f', '--func', nargs=1, 
+parser.add_argument('-f', '--func', nargs=1, required=True,
                     help='TPMS custom function (e.g. python custom.py -f "cos(2*pi*x) + cos(2*pi*y) + cos(2*pi*z)")')
 
 args = parser.parse_args()
 
-# function  = "cos(2*pi*x) + cos(2*pi*y) + cos(2*pi*z)"
 function = args.func[0]
-print(function)
-
+if function == 'default':
+    function = "cos(2*pi*x) + cos(2*pi*y) + cos(2*pi*z)"
 
 # Size of the mesh
 size_mesh = 0.03
@@ -29,7 +27,7 @@ revel = Rve(a, b, c, size_mesh)
 elem = BasicGeometry(number=0, shape='tpms',
                      xc=0.5  , yc=0.5  , zc=0.5,
                      psi=0.  , theta=0., phi=0.,
-                     param_geom={"type_surface": surface,
+                     param_geom={"type_surface": 'custom',
                                  "type_part"   : part,
                                  "thickness"   : thickness,
                                  "function"    : function},
