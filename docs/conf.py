c92443b7b08695f434c8eb45ece77894c4d97750
Kevin Marchais <kevinmarchais@gmail.com>
2022-05-25 23:19:36 +0200

    Revert "doc"
    This reverts commit e903851647d0da319e9d3e3f26b5a8c1f7af55bc.

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ master_doc = 'index'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.coverage',
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.coverage', 
               'sphinx.ext.napoleon', 'sphinx.ext.autosummary']
 
 # Add any paths that contain templates here, relative to this directory.
@@ -57,4 +57,4 @@ html_theme = 'sphinxdoc'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = []
+html_static_path = ['_static']
