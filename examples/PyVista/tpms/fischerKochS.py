25ffb869b8c23c33a090cb174e70cb9394d7bb9c
Yves Chemisky <yves.chemisky@gmail.com>
2022-06-29 18:27:23 +0200

    Create fischerKochS.py

---
+++ examples/PyVista/tpms/fischerKochS.py
@@ -0,0 +1,12 @@
+from microgen import Tpms, tpms
+import pyvista as pv
+
+geometry = Tpms(
+    surface_function=tpms.fischerKochS,
+    repeat_cell=5
+)
+mesh = geometry.generateSurfaceVtk()
+
+pl = pv.Plotter()
+pl.add_mesh(mesh)
+pl.show()
