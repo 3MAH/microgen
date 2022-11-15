8395ebd73a0ee5b7593dd05805d2d2b6d2cb04a2
Kevin Marchais <kevinmarchais@gmail.com>
2022-11-15 13:11:21 +0100

    added density grading + test new way to generate tpms volume

--- projects/grading/density.py
+++ projects/grading/density.py
@@ -4,93 +4,116 @@ import numpy as np
 import pyvista as pv
 import cadquery as cq
 
+import sys
+
 import itertools
 
-def createSurfaces(
-    geometry,
-    isovalues: list[float],
-    resolution: int = 20,
-    smoothing: int = 100,
-) -> list[cq.Shell]:
-    """
-    Create TPMS surfaces for the corresponding isovalue, return a list of cq.Shell
+def createShell(mesh: pv.PolyData) -> cq.Shell:
+    list_of_triangles = mesh.faces.reshape(-1, 4)[:, 1:]
+    list_of_triangles = np.c_[list_of_triangles, list_of_triangles[:, 0]]
 
-    :param numsber_surfaces: number of surfaces
-    :param isovalues: height isovalues of the given tpms function
-    :param resolution: surface file name
-    :param smoothing: smoothing loop iterations
-    """
+    faces = []
+    for i in range(len(list_of_triangles)):
+        ixs = list_of_triangles[i]
+        print(f"\t{i}/{len(list_of_triangles)}", end="\r")
+        lines = []
+        for v1, v2 in zip(ixs[:], ixs[1:]):
+            vertice_coords1 = mesh.points[v1]
+            vertice_coords2 = mesh.points[v2]
+            lines.append(
+                cq.Edge.makeLine(
+                    cq.Vector(*vertice_coords1), cq.Vector(*vertice_coords2)
+                )
+            )
+        wire = cq.Wire.assembleEdges(lines)
+        faces.append(cq.Face.makeFromWires(wire))
+    return cq.Shell.makeShell(faces)
 
-    grid, surface = geometry.computeSurfaceFunction(geometry.surface_function, resolution)
+def h(x, y, z, cell_size):
+    hmin = 0.05
+    hmax = 0.25
+    L = cell_size[0]
 
-    shells = []
-    for isovalue in isovalues:
-        mesh = geometry.computeSurfaceVtk(grid, surface, isovalue, smoothing)
-        shells.append(geometry.createShell(mesh))
-    return shells
+    return (hmax - hmin) * x / L + 0.5 * (hmin + hmax)
 
-def generate(
-    geometry,
-    resolution: int = 20,
-    smoothing: int = 100,
-) -> cq.Shape:
-    """
-    Creates thick TPMS geometry (sheet or skeletal part) from surface
+cell_size = (5, 5, 1)
+repeat_cell = (5, 5, 1)
+resolution = 20
 
-    :param resolution: surface file name
-    :param smoothing: smoothing loop iterations
-    """
-
-    isovalues = [
-        -geometry.thickness / 2.,
-        -geometry.thickness / 6.0,
-        geometry.thickness / 6.0,
-        geometry.thickness / 2.,
-    ]
-    shells = createSurfaces(geometry, isovalues=isovalues, resolution=resolution, smoothing=smoothing)
-
-    face_cut_tp = shells[2]
-    face_cut_tm = shells[1]
-    face_cut_p = shells[3]
-    face_cut_m = shells[0]
-
-    box_wp = cq.Workplane("front").box(1, 1, 1)
-
-    boxCut_wp = box_wp.split(face_cut_p)
-    boxCut_wp = boxCut_wp.split(face_cut_m)
-
-    boxWorkplanes: list[cq.Workplane] = boxCut_wp.solids().all()
-
-    listShapes: list[tuple[int, cq.Shape]] = []
-    for wp in boxWorkplanes:
-        n_shapes = wp.split(face_cut_tp).split(face_cut_tm).solids().size()
-        shape = wp.val()
-        listShapes.append((n_shapes, shape))
-
-    if geometry.type_part == "sheet":
-        sheet = [shape for (n_shapes, shape) in listShapes if n_shapes > 1]
-        to_fuse = [cq.Shape(shape.wrapped) for shape in sheet]
-        shape = fuseShapes(to_fuse, True)
-    elif geometry.type_part == "skeletal":
-        skeletal = [shape for (n_shapes, shape) in listShapes if n_shapes == 1]
-        shape = skeletal[0]
-        # to_fuse = [cq.Shape(shape.wrapped) for shape in skeletal]
-        # shape = fuseShapes(to_fuse, False)
-    return shape
-
-
-geometry = Tpms(
-    surface_function=tpms.gyroid,
-    type_part="sheet",
-    thickness=0.2,
-    repeat_cell=1,
-    cell_size=1
+res = (resolution * repeat_cell[0], resolution * repeat_cell[1], resolution * repeat_cell[2])
+x_min, y_min, z_min = -0.5 * cell_size[0], -0.5 * cell_size[1], -0.5 * cell_size[2]
+grid = pv.UniformGrid(
+    dims=res,
+    spacing=(cell_size[0] / (res[0] - 1),
+             cell_size[1] / (res[1] - 1),
+             cell_size[2] / (res[2] - 1)),
+    origin=(x_min, y_min, z_min),
 )
+x, y, z = grid.points.T
 
-shape = generate(geometry)
-shape = pv.PolyData(shape.toVtkPolyData(tolerance=0.01, angularTolerance=0.1, normals=True))
+surface = tpms.gyroid(x, y, z, repeat_cell, cell_size)
+# surface = tpms.schwarzP(x, y, z, repeat_cell, cell_size)
+# surface = tpms.schwarzD(x, y, z, repeat_cell, cell_size)
+# surface = tpms.schoenFRD(x, y, z, repeat_cell, cell_size)
+# surface = tpms.schoenIWP(x, y, z, repeat_cell, cell_size)
+# surface = tpms.neovius(x, y, z, repeat_cell, cell_size) # Error
+# surface = tpms.honeycomb(x, y, z, repeat_cell, cell_size) # bizarre
+# surface = tpms.pmy(x, y, z, repeat_cell, cell_size) # bizarre
+# surface = tpms.fischerKochS(x, y, z, repeat_cell, cell_size) # bizarre
+# surface = tpms.lidinoid(x, y, z, repeat_cell, cell_size) # ERROR
+
+thickness = 2 * np.pi * h(x, y, z, cell_size)
+
+surf_p = surface + thickness / 2.
+mesh_p = grid.contour(isosurfaces=[0], scalars=surf_p)
+mesh_p.smooth(n_iter=100)
+mesh_p.clean(inplace=True)
+shell_p = createShell(mesh_p)
+print("shell_p")
+
+surf_m = surface - thickness / 2.
+mesh_m = grid.contour(isosurfaces=[0], scalars=surf_m)
+mesh_m.smooth(n_iter=100)
+mesh_m.clean(inplace=True)
+shell_m = createShell(mesh_m)
+print("shell_m")
+
+
+box = cq.Workplane("front").box(cell_size[0], cell_size[1], cell_size[2])
+box_cut = box.split(shell_p).split(shell_m)
+
+print("cut")
+
+solids = box_cut.solids().all()
+print(len(solids))
+
+if len(solids) % 2 == 0: # assume that the number of solids is odd
+    print("ERROR")
+    sys.exit()
+
+middle_solid_number = (len(solids) - 1) // 2
+if (middle_solid_number) % 2 == 0: 
+    sheet = [cq.Shape(solids[even].val().wrapped) for even in range(0, len(solids), 2)]
+    skeletal = [cq.Shape(solids[odd].val().wrapped) for odd in range(1, len(solids), 2)]
+else:
+    skeletal = [cq.Shape(solids[even].val().wrapped) for even in range(0, len(solids), 2)]
+    sheet = [cq.Shape(solids[odd].val().wrapped) for odd in range(1, len(solids), 2)]
+
+print("sheet and skeletal separated")
+
+sheet = cq.Compound.makeCompound(sheet)
+sheet.exportStl("sheet.stl")
+skeletal = [cq.Compound.makeCompound(skeletal[:len(skeletal)//2]), cq.Compound.makeCompound(skeletal[len(skeletal)//2:])]
+
+print("converting to polydata")
+pd_sheet = pv.PolyData(sheet.toVtkPolyData(tolerance=0.01, angularTolerance=0.1, normals=True))
+pd_skeletal_m = pv.PolyData(skeletal[0].toVtkPolyData(tolerance=0.01, angularTolerance=0.1, normals=True))
+pd_skeletal_p = pv.PolyData(skeletal[1].toVtkPolyData(tolerance=0.01, angularTolerance=0.1, normals=True))
+
 p = pv.Plotter()
-p.add_mesh(shape, color='w')
+p.add_mesh(pd_sheet, color='w')
+p.add_mesh(pd_skeletal_m, color='b')
+p.add_mesh(pd_skeletal_p, color='r')
 p.camera_position = 'xy'
 p.parallel_projection = True 
 p.show_axes()
