a1d3e2b0e97e67c597d423b6a2a959d49c03e799
ricardo0115 <ricardoguevara93@gmail.com>
2023-04-26 16:12:17 +0200

    Replaced old `setup.py` by `pyproject.toml`.
    Minimal dependencies declared in `pyproject.toml`

--- setup.py
+++ setup.py
@@ -1,18 +1,3 @@
-#!/usr/bin/env python
+from setuptools import setup
 
-from setuptools import setup, find_packages
-
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
-
-setup(name='microgen',
-      version='1.0.1',
-      description='Microstructure generation',
-      long_description=long_description,
-      long_description_content_type="text/markdown",
-      author="3MAH",
-      author_email="set3mah@gmail.com",
-      url='https://github.com/3MAH/microgen',
-      packages=find_packages(),
-      python_requires=">=3.9",
-)
+setup()
