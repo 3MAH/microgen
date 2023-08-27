28614509b96450415b3a9f698d92424000d9be7c
Kevin Marchais <kevinmarchais@gmail.com>
2023-08-27 17:00:28 +0200

    fix doc

--- microgen/shape/__init__.py
+++ microgen/shape/__init__.py
@@ -7,11 +7,12 @@ Shape (:mod:`microgen.shape`)
    :hide-code:
 
    import pyvista
-   pyvista.set_jupyter_backend('pythreejs')
+   pyvista.set_jupyter_backend('static')
    pyvista.global_theme.background = 'white'
-   pyvista.global_theme.window_size = [690, 400]
-   pyvista.global_theme.antialiasing = True
+   pyvista.global_theme.window_size = [600, 400]
    pyvista.global_theme.axes.show = False
+   pyvista.global_theme.smooth_shading = True
+   pyvista.global_theme.antialiasing = 'fxaa'
 
 """
 
