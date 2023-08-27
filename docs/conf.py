486a031e2c92660d43d05c9546b32a8e1b4c88f5
Kevin Marchais <kevinmarchais@gmail.com>
2023-08-27 11:43:03 +0200

    fix doc environment

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = '2022, 3MAH'
 author = '3MAH'
 
 # The full version, including alpha/beta/rc tags
-release = '1.0.0'
+release = '1.0.1'
 
 
 # -- General configuration ---------------------------------------------------
