7f73eed5ba2f182ee937164041326776447104b0
Yves Chemisky <yves.chemisky@gmail.com>
2023-08-31 16:53:41 +0200

    Update doc

--- microgen/boxMesh.py
+++ microgen/boxMesh.py
@@ -10,7 +10,7 @@ import numpy as np
 import warnings
 from scipy.spatial import KDTree
 from .rve import Rve
-from .phaseMesh import PhaseMesh
+from .phaseMesh import PhaseMesh, _check_if_only_linear_tetrahedral
 
 # try: 
 #     import trimesh    
@@ -28,6 +28,7 @@ class BoxMesh(PhaseMesh):
     :param elm_type : type of elements (np.ndarray)
     :param nodes_index : index of node list (if different from the natural index of nodes array)   
     """
+
     def __init__(
         self,
         nodes : np.ndarray,
@@ -79,16 +80,29 @@ class BoxMesh(PhaseMesh):
         self.faces = []
 
     @staticmethod
-    def from_pyvista(pvmesh, name = ""):
-        """Build a Mesh from a pyvista UnstructuredGrid or PolyData mesh. 
-        Node and element data are not copied.
-        Mesh with multiple element type are handled."""                       
+    def from_pyvista(
+        pvmesh : pv.UnstructuredGrid,
+        name : str = None):
+        """Build a PhaseMesh from a pyvista UnstructuredGrid or PolyData mesh (in this last case, 
+        the PolyData object is cast into a Unstructured Grid). 
+        Node and element data are not copied (by default a shallow copy is operated)
+        Mesh with multiple element type are not handled.
+        Note that for now phaseMesh works only considering tetrahedral elements        
 
+        :param pvmesh: the mesh as a pyvista UnstructuredGrid object
+
+        :return : A PhaseMesh Object from the tetrahedral elements of the pyvista Unistructured Grid
+        """                       
         if isinstance(pvmesh, pv.PolyData):
             pvmesh = pvmesh.cast_to_unstructured_grid()
-        
-        elements = pvmesh.cells_dict
-        return BoxMesh(pvmesh.points, elements)
+                
+        try: 
+            #extract only the tetrahedral elements            
+            _check_if_only_linear_tetrahedral(pvmesh)
+            elements = {10: pvmesh.cells_dict[10]}
+            return BoxMesh(pvmesh.points, elements, pvmesh, name)            
+        except ValueError as e:     
+            print(e)
 
     def construct(
         self,
@@ -98,7 +112,7 @@ class BoxMesh(PhaseMesh):
         """
         Construct a box Mesh with list of points in faces (excluding edges), edges (excluding corners) and corners.
 
-        :param rve: RVE of the box
+        :param rve: RVE of the box microgen.Rve
         :param tol: tolerance to find the points on a face or a edge
         """            
         if(isinstance(rve, Rve) == False) : 
@@ -209,6 +223,8 @@ class BoxMesh(PhaseMesh):
     ) -> Rve:
         """
         Return a representative volume element (Rve) of the considered mesh
+
+        :return Rve: a Rve object attached to the considered BoxMesh object
         """            
 
         if(isinstance(self._rve, Rve)):
@@ -222,9 +238,9 @@ class BoxMesh(PhaseMesh):
         value: Rve,
     ) -> None:
         """
-        set a RVE value
+        set a representative volume element (Rve) of the considered mesh
 
-        :param value: Rve of the box to set
+        :param value: Rve of the box to set        
         """            
 
         self._rve = value
@@ -233,7 +249,7 @@ class BoxMesh(PhaseMesh):
         self,
     ) -> Rve:
         """
-        build a RVE value from the mesh bounding box
+        build a a representative volume element (Rve) of the considered mesh from its bounding box
 
         :return rve: RVE of the mesh bounding box
         """          
@@ -241,14 +257,6 @@ class BoxMesh(PhaseMesh):
             self._mesh = self.to_pyvista()
         xmin, xmax, ymin, ymax, zmin, zmax = self._mesh.bounds
         return Rve.from_min_max(xmin, xmax, ymin, ymax, zmin, zmax)
-
-    # def _is_intersection_node_coincide(
-    #         intersection_node : np.array,
-    #         node_list : np.array,
-    #         threshold_distance : float=1.E-6,
-    # ) -> bool:
-    #     distances = np.linalg.norm(node_list - intersection_node, axis=1)
-    #     return np.any(distances < threshold_distance)
 
     def _closest_points_on_faces(
         self,
@@ -260,9 +268,17 @@ class BoxMesh(PhaseMesh):
         """
         Find the closest points on a opposite face to write interpolation relationship
         if a displacement condition between pair nodes is defined on such opposite surfaces
+        It takes the set of points on faces 'face_Xm', 'face_Ym', 'face_Zm' (excluding edges and corners)
+        and returns for each opposite face the closest points (index of the points, distance) of the
+        projected point on the corresponding opposite face
 
         :param k_neighbours : number of closest points
         :param rve : RVE of the mesh bounding box. if None, the rve is built from the mesh bounding box
+
+        :return dict: a dictionnary with (np.array) of indices and an np.array of distances for each neigboor:
+            'face_Xp' : (index[0], dist[0]), 
+            'face_Yp' : (index[1], dist[1]), 
+            'face_Zp' : (index[2], dist[2])
         """                            
             
         if(isinstance(rve, Rve) == False) : 
@@ -305,6 +321,7 @@ class BoxMesh(PhaseMesh):
             else:
                 dist_temp_list = dist_temp.tolist()
 
+            #If pair nodes exist (opposite node exactly match), return only the pair node (the closest neighbour)
             if k_neighbours>1:
                 for i in range(0, len(dist_temp)):
                     if dist_temp_list[i][0]<tol:
@@ -478,6 +495,7 @@ class BoxMesh(PhaseMesh):
         """
         Find the cells to which a given point belong to while using a ray tracing normal to the face on which it belongs
         :param rve : RVE of the mesh bounding box. if None, the rve is built from the mesh bounding box
+        :param tol : tolerance to evaluate the threshold between the cells and the boundary RVE of the mesh bounding box. if None, the rve is built from the mesh bounding box        
         """    
 
         if(isinstance(rve, Rve) == False) : 
@@ -501,7 +519,7 @@ class BoxMesh(PhaseMesh):
         surface = self.surface 
         surface["CellIDs"] = np.arange(surface.n_cells)
 
-        list_p = []
+        list_cells_for_each_face = []
         list_ray_trace = []
 
         for i, face in enumerate(faces_m):
@@ -510,7 +528,7 @@ class BoxMesh(PhaseMesh):
             surface.compute_implicit_distance(plane, inplace=True)      
 
             surface_p = surface.threshold([-tol, tol], all_scalars=True, scalars='implicit_distance').extract_surface()
-            list_p.append(surface_p["CellIDs"])
+            list_cells_for_each_face.append(surface_p["CellIDs"])
 
             directions = np.tile(normals[i], (np.shape(faces_m[i])[0],1))
 
@@ -533,8 +551,8 @@ class BoxMesh(PhaseMesh):
             list_ray_trace.append(raytraceresult)
 
         return {
-            'face_Xp' : (list_p[0], list_ray_trace[0][0], list_p[0][list_ray_trace[0][2]]),
-            'face_Yp' : (list_p[1], list_ray_trace[1][0], list_p[1][list_ray_trace[1][2]]), 
-            'face_Zp' : (list_p[2], list_ray_trace[2][0], list_p[2][list_ray_trace[2][2]])
+            'face_Xp' : (list_cells_for_each_face[0], list_ray_trace[0][0], list_cells_for_each_face[0][list_ray_trace[0][2]]),
+            'face_Yp' : (list_cells_for_each_face[1], list_ray_trace[1][0], list_cells_for_each_face[1][list_ray_trace[1][2]]), 
+            'face_Zp' : (list_cells_for_each_face[2], list_ray_trace[2][0], list_cells_for_each_face[2][list_ray_trace[2][2]])
         }    
        
