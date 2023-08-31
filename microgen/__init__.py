658df528b5676248263cb3a44e1a21a575fdaea3
Kevin Marchais <kevinmarchais@gmail.com>
2023-08-31 11:18:15 +0200

    define version in project metada, import it in __init__.py

--- microgen/__init__.py
+++ microgen/__init__.py
@@ -1,3 +1,5 @@
+import importlib.metadata
+
 from .external import *
 from .mesh import *
 from .operations import *
@@ -6,5 +8,4 @@ from .phase import *
 from .rve import *
 from .shape import *
 
-version_info = 1, 0, 1
-__version__ = '.'.join(map(str, version_info))
+__version__ = importlib.metadata.version(__package__ or __name__)
