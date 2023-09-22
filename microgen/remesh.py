e92953462c5774e34379e63b8d6b41737a111259
Yasmin Legerstee <125015797+ylgrst@users.noreply.github.com>
2023-09-22 17:26:19 +0200

    Fix another bug where TemporaryFile and str were mixed up

--- microgen/remesh.py
+++ microgen/remesh.py
@@ -4,14 +4,15 @@ from tempfile import NamedTemporaryFile
 from microgen import BoxMesh, Mmg
 import meshio
 
+
 def remesh_keeping_periodicity_for_fem(
-    input_mesh: BoxMesh,
-    output_mesh_file: str,
-    hausd: float = None,
-    hgrad: float = None,
-    hmax: float = None,
-    hmin: float = None,
-    hsiz: float = None,
+        input_mesh: BoxMesh,
+        output_mesh_file: str,
+        hausd: float = None,
+        hgrad: float = None,
+        hmax: float = None,
+        hmin: float = None,
+        hsiz: float = None,
 ) -> None:
     """
     Remeshes a mesh (.mesh file format) derived from a BoxMesh using mmg while keeping periodicity
@@ -27,12 +28,12 @@ def remesh_keeping_periodicity_for_fem(
     :param hsiz: Build a constant size map of size hsiz
     """
     with NamedTemporaryFile(
-        suffix=".mesh", delete=False
+            suffix=".mesh", delete=False
     ) as boundary_triangles_file, NamedTemporaryFile(suffix='mesh', delete=False) as raw_output_mesh_file:
         _generate_mesh_with_required_triangles(input_mesh, boundary_triangles_file.name)
         Mmg.mmg3d(
             input=boundary_triangles_file.name,
-            output=raw_output_mesh_file,
+            output=raw_output_mesh_file.name,
             hausd=hausd,
             hgrad=hgrad,
             hmax=hmax,
@@ -42,48 +43,53 @@ def remesh_keeping_periodicity_for_fem(
         os.remove(
             boundary_triangles_file.name
         )  # to solve compatibility issues of NamedTemporaryFiles with Windows
-    os.remove(raw_output_mesh_file.name.replace(".mesh",".sol")) # Remove unused .sol file created by mmg
+    os.remove(raw_output_mesh_file.name.replace(".mesh", ".sol"))  # Remove unused .sol file created by mmg
     _mesh_cleaner(raw_output_mesh_file.name, output_mesh_file)
 
 
-def _generate_mesh_with_required_triangles(input_mesh : BoxMesh, mesh_including_required_triangles : str = 'merged_reqtri.mesh') -> None:
-    with NamedTemporaryFile(suffix='.vtk', delete=False) as vtk_file, NamedTemporaryFile(suffix='.mesh', delete=False) as mesh_file:
+def _generate_mesh_with_required_triangles(input_mesh: BoxMesh,
+                                           mesh_including_required_triangles: str = 'merged_reqtri.mesh') -> None:
+    with NamedTemporaryFile(suffix='.vtk', delete=False) as vtk_file, NamedTemporaryFile(suffix='.mesh',
+                                                                                         delete=False) as mesh_file:
         _generate_vtk_with_boundary_triangles(input_mesh, vtk_file.name)
         _convert_vtk_to_mesh(vtk_file.name, mesh_file.name)
         _add_required_triangles_to_mesh_file(input_mesh, mesh_file.name, mesh_including_required_triangles)
 
 
-def _generate_vtk_with_boundary_triangles(input_mesh : BoxMesh, output_mesh : str = 'merged.vtk') -> None:
+def _generate_vtk_with_boundary_triangles(input_mesh: BoxMesh, output_mesh: str = 'merged.vtk') -> None:
     pyvista_mesh = input_mesh.to_pyvista()
     mesh_boundary, _ = input_mesh.boundary_elements(input_mesh.rve)
     merged_mesh = pyvista_mesh.merge(mesh_boundary)
     merged_mesh.save(output_mesh, binary=False)
 
-def _convert_vtk_to_mesh(input_vtk_file : str, output_mesh_file : str) -> None:
+
+def _convert_vtk_to_mesh(input_vtk_file: str, output_mesh_file: str) -> None:
     meshio_mesh = meshio.read(input_vtk_file)
     meshio_mesh.write(output_mesh_file)
 
 
-def _get_number_of_boundary_triangles_from_boxmesh(input_mesh : BoxMesh) -> int:
+def _get_number_of_boundary_triangles_from_boxmesh(input_mesh: BoxMesh) -> int:
     mesh_boundary, _ = input_mesh.boundary_elements(input_mesh.rve)
     n_boundary_triangles = mesh_boundary.n_faces
 
     return n_boundary_triangles
 
-def _add_required_triangles_to_mesh_file(input_mesh : BoxMesh, input_mesh_file : str, output_mesh_file : str) -> None:
+
+def _add_required_triangles_to_mesh_file(input_mesh: BoxMesh, input_mesh_file: str, output_mesh_file: str) -> None:
     n_required_triangles = _get_number_of_boundary_triangles_from_boxmesh(input_mesh)
     with open(input_mesh_file, 'r') as input_file:
-        lines = input_file.readlines()[:-1] #remove last line End
+        lines = input_file.readlines()[:-1]  # remove last line End
 
     with open(output_mesh_file, 'w+') as output_file:
         output_file.writelines(lines)
         output_file.write("RequiredTriangles\n")
         output_file.write(str(n_required_triangles) + "\n")
         for i in range(n_required_triangles):
-            output_file.write(str(i+1) + "\n")
+            output_file.write(str(i + 1) + "\n")
         output_file.write("End\n")
 
-def _mesh_cleaner(input_mesh_file : str, output_mesh_file : str, mesh_version : int, dimension : int) -> None:
+
+def _mesh_cleaner(input_mesh_file: str, output_mesh_file: str, mesh_version: int, dimension: int) -> None:
     with open(input_mesh_file, 'r') as input_file:
         lines = input_file.readlines()
 
@@ -100,6 +106,7 @@ def _mesh_cleaner(input_mesh_file : str, output_mesh_file : str, mesh_version : 
             if write_bool:
                 output_file.write(line)
         output_file.write('End\n')
+
 
 def _only_numbers_in_line(str_list: list[str]) -> bool:
     return all(not flag.isalpha() for flag in str_list)
