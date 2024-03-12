d5afc64a22baa0294c1d5277d8e5e8d18209b024
Yasmin Legerstee <125015797+ylgrst@users.noreply.github.com>
2024-03-12 11:44:32 +0100

    Add mmg installation check to examples remesh.py

--- examples/Mesh/remesh/remesh.py
+++ examples/Mesh/remesh/remesh.py
@@ -1,4 +1,5 @@
 import os
+import subprocess
 
 import pyvista as pv
 
@@ -6,16 +7,29 @@ from microgen import BoxMesh, Tpms
 from microgen.remesh import remesh_keeping_periodicity_for_fem
 from microgen.shape.surface_functions import gyroid
 
-if "data" not in os.listdir("."):
-    os.mkdir("data")
+USE_MMG = False
+try:
+    subprocess.run(["mmg3d_O3", "-h"])
+    USE_MMG = True
+except Exception:
+    print("mmg command did not work, check if it is installed or contact a developer")
 
-gyroid_vtk = pv.UnstructuredGrid(
-    Tpms(surface_function=gyroid, offset=1.0, resolution=50).generateVtk(
-        type_part="sheet"
+if USE_MMG:
+    if "data" not in os.listdir("."):
+        os.mkdir("data")
+
+    print("generate gyroid", flush=True)
+    gyroid_vtk = pv.UnstructuredGrid(
+        Tpms(surface_function=gyroid, offset=1.0, resolution=50).generateVtk(
+            type_part="sheet"
+        )
     )
-)
-gyroid_vtk.save("data/initial_gyroid_mesh.vtk")
-gyroid_mesh = BoxMesh.from_pyvista(gyroid_vtk)
+    print("save gyroid", flush=True)
+    gyroid_vtk.save("data/initial_gyroid_mesh.vtk")
+    print("convert gyroid", flush=True)
+    gyroid_mesh = BoxMesh.from_pyvista(gyroid_vtk)
 
-remeshed_gyroid_mesh = remesh_keeping_periodicity_for_fem(gyroid_mesh, hmax=0.02)
-remeshed_gyroid_mesh.save("data/remeshed_gyroid_mesh.vtk")
+    print("remesh gyroid", flush=True)
+    remeshed_gyroid_mesh = remesh_keeping_periodicity_for_fem(gyroid_mesh, hmax=0.02)
+    print("save remeshed gyroid", flush=True)
+    remeshed_gyroid_mesh.save("data/remeshed_gyroid_mesh.vtk")
