26ad687b1af43625388b3cb7b8ae4e556e44b0db
ricardo0115 <ricardoguevara93@gmail.com>
2024-05-03 19:45:53 +0200

    Update example

--- examples/TPMS/surface/fischerKochS.py
+++ examples/TPMS/surface/fischerKochS.py
@@ -3,7 +3,7 @@ from pathlib import Path
 from microgen import Tpms
 from microgen.shape.surface_functions import fischerKochS
 
-geometry = Tpms(surface_function=fischerKochS, repeat_cell=5)
+geometry = Tpms(surface_function=fischerKochS, repeat_cell=5, offset=0)
 mesh = geometry.generate_vtk(type_part="surface")
 
 vtk_file = Path(__file__).parent / "surface.vtk"
