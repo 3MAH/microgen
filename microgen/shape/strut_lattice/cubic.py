b6aa0bd02b4017cd5a6ab50a72db56ffd4b137d0
ylgrst <125015797+ylgrst@users.noreply.github.com>
2025-03-12 22:05:26 +0100

    Refactor cubic.py

--- microgen/shape/strut_lattice/cubic.py
+++ microgen/shape/strut_lattice/cubic.py
@@ -1,67 +1,34 @@
-from .abstract_lattice import AbstractLattice
 import numpy as np
 import numpy.typing as npt
+from scipy.spatial import KDTree
+import itertools
+from .abstract_lattice import AbstractLattice
 
 _STRUT_NUMBER = 12
 _STRUT_HEIGHTS = 1.0
+_VERTICES = np.array(list(itertools.product([-0.5, 0.5], repeat=3)))
+_STRUT_VERTICES_PAIRS = np.array([
+    [i, j] for i in range(len(_VERTICES)) 
+    for j in KDTree(_VERTICES).query_ball_point(_VERTICES[i], r=_STRUT_HEIGHTS) if i < j
+])
 
 class Cubic(AbstractLattice):
     """
-Class to create a unit cubic lattice of given cell size and density or strut radius
+    Class to create a unit cubic lattice of given cell size and density or strut radius.
     """
-    
-    def __init__(self,
-                 *args, **kwargs
-                 ) -> None:
 
+    def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs, strut_number=_STRUT_NUMBER, strut_heights=_STRUT_HEIGHTS)
 
-
     def _compute_vertices(self) -> npt.NDArray[np.float64]:
-        vertices_array = self.center + self.cell_size * np.array([
-            [0.5, -0.5, 0.5],
-            [0.5, 0.5, 0.5],
-            [-0.5, 0.5, 0.5],
-            [-0.5, -0.5, 0.5],
-            [0.5, -0.5, -0.5],
-            [0.5, 0.5, -0.5],
-            [-0.5, 0.5, -0.5],
-            [-0.5, -0.5, -0.5]
-        ])
+        """Compute the vertices of the cubic lattice."""
+        return self.center + self.cell_size * _VERTICES
 
-        return vertices_array
-    
     def _compute_strut_centers(self) -> npt.NDArray[np.float64]:
-        centers_array = np.array([
-            (self.vertices[0] + self.vertices[1]),
-            (self.vertices[0] + self.vertices[4]),
-            (self.vertices[0] + self.vertices[3]),
-            (self.vertices[1] + self.vertices[5]),
-            (self.vertices[1] + self.vertices[2]),
-            (self.vertices[2] + self.vertices[3]),
-            (self.vertices[2] + self.vertices[6]),
-            (self.vertices[3] + self.vertices[7]),
-            (self.vertices[4] + self.vertices[5]),
-            (self.vertices[4] + self.vertices[7]),
-            (self.vertices[5] + self.vertices[6]),
-            (self.vertices[6] + self.vertices[7])
-        ]) / 2.0
-        return centers_array
-    
-    def _compute_strut_directions(self) -> npt.NDArray[np.float64]:
-        directions_array = np.array([
-            (self.vertices[1] - self.vertices[0]) / np.linalg.norm((self.vertices[1] - self.vertices[0])),
-            (self.vertices[4] - self.vertices[0]) / np.linalg.norm((self.vertices[4] - self.vertices[0])),
-            (self.vertices[3] - self.vertices[0]) / np.linalg.norm((self.vertices[3] - self.vertices[0])),
-            (self.vertices[5] - self.vertices[1]) / np.linalg.norm((self.vertices[5] - self.vertices[1])),
-            (self.vertices[2] - self.vertices[1]) / np.linalg.norm((self.vertices[2] - self.vertices[1])),
-            (self.vertices[3] - self.vertices[2]) / np.linalg.norm((self.vertices[3] - self.vertices[2])),
-            (self.vertices[6] - self.vertices[2]) / np.linalg.norm((self.vertices[6] - self.vertices[2])),
-            (self.vertices[7] - self.vertices[3]) / np.linalg.norm((self.vertices[7] - self.vertices[3])),
-            (self.vertices[5] - self.vertices[4]) / np.linalg.norm((self.vertices[5] - self.vertices[4])),
-            (self.vertices[7] - self.vertices[4]) / np.linalg.norm((self.vertices[7] - self.vertices[4])),
-            (self.vertices[6] - self.vertices[5]) / np.linalg.norm((self.vertices[6] - self.vertices[5])),
-            (self.vertices[7] - self.vertices[6]) / np.linalg.norm((self.vertices[7] - self.vertices[6]))
-        ])
+        """Compute the centers of the struts."""
+        return np.mean(self.vertices[_STRUT_VERTICES_PAIRS], axis=1)
 
-        return directions_array
+    def _compute_strut_directions(self) -> npt.NDArray[np.float64]:
+        """Compute the normalized direction vectors of the struts."""
+        vectors = np.diff(self.vertices[_STRUT_VERTICES_PAIRS], axis=1).squeeze()
+        return vectors / np.linalg.norm(vectors, axis=1, keepdims=True)
