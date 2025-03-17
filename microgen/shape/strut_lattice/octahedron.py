d410655fa274274b1fc8e732aaa4b0c6d68f8aca
ylgrst <125015797+ylgrst@users.noreply.github.com>
2025-03-17 11:33:21 +0100

    Refactor octahedron.py

--- microgen/shape/strut_lattice/octahedron.py
+++ microgen/shape/strut_lattice/octahedron.py
@@ -1,29 +1,8 @@
 from .abstract_lattice import AbstractLattice
 import numpy as np
 import numpy.typing as npt
-from typing import List, Tuple
 import math as m
 from scipy.spatial import KDTree
-
-_UNIT_CUBE_SIZE = 1.0
-_STRUT_NUMBER = 12
-_STRUT_HEIGHTS = m.sqrt(2.0) / 2.0
-
-_BASE_VERTICES = np.array([
-            [sign * _UNIT_CUBE_SIZE/2.0 if i == axis else 0.0 for i in range(3)]
-            for axis in range(3) for sign in [-1, 1]
-        ])
-
-def _compute_vertex_pairs() -> List[Tuple[int, int]]:
-    tree = KDTree(_BASE_VERTICES)
-    pairs = set()
-    for i in range(len(_BASE_VERTICES)):
-        _, indices = tree.query(_BASE_VERTICES[i], k=5)
-        for j in indices[1:]:
-            pairs.add(tuple(sorted((i, j))))
-    return np.array(list(pairs))
-
-_VERTEX_PAIRS = _compute_vertex_pairs()
 
 class Octahedron(AbstractLattice):
     """
@@ -33,17 +12,34 @@ class Octahedron(AbstractLattice):
     def __init__(self,
                  *args, **kwargs
                  ) -> None:
-        super().__init__(*args, **kwargs, strut_number=_STRUT_NUMBER, strut_heights=_STRUT_HEIGHTS)
+        self._base_vertices = self._generate_base_vertices()
+        self._strut_vertex_pairs = self._generate_vertex_pairs()
+        super().__init__(*args, **kwargs, strut_number=12, strut_heights=m.sqrt(2.0) / 2.0)
+    
+    def _generate_base_vertices(self) -> npt.NDArray[np.float64]:
+        return np.array([
+            [sign * self._UNIT_CUBE_SIZE/2.0 if i == axis else 0.0 for i in range(3)]
+            for axis in range(3) for sign in [-1, 1]
+        ])
         
     def _compute_vertices(self) -> npt.NDArray[np.float64]:
         """Compute the vertices of the octahedric lattice."""
-        return self.center + self.cell_size * _BASE_VERTICES
+        return self.center + self.cell_size * self._base_vertices
+    
+    def _generate_vertex_pairs(self) -> npt.NDArray[int]:
+        tree = KDTree(self._base_vertices)
+        pairs = set()
+        for i in range(len(self._base_vertices)):
+            _, indices = tree.query(self._base_vertices[i], k=5)
+            for j in indices[1:]:
+                pairs.add(tuple(sorted((i, j))))
+        return np.array(list(pairs))
     
     def _compute_strut_centers(self) -> npt.NDArray[np.float64]:
         """Compute the centers of the struts."""
-        return np.mean(self.vertices[_VERTEX_PAIRS], axis=1)
+        return np.mean(self.vertices[self._strut_vertex_pairs], axis=1)
     
     def _compute_strut_directions(self) -> npt.NDArray[np.float64]:
         """Compute the normalized direction vectors of the struts."""
-        vectors = np.diff(self.vertices[_VERTEX_PAIRS], axis=1).squeeze()
+        vectors = np.diff(self.vertices[self._strut_vertex_pairs], axis=1).squeeze()
         return vectors / np.linalg.norm(vectors, axis=1, keepdims=True)
