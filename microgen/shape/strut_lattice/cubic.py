ae447a5ac7ecf6eb95b9d8855e5f5985f9e90852
ylgrst <125015797+ylgrst@users.noreply.github.com>
2025-03-17 10:31:30 +0100

    Refactor cubic.py
    Generate vertex pairs using functional programming

--- microgen/shape/strut_lattice/cubic.py
+++ microgen/shape/strut_lattice/cubic.py
@@ -4,14 +4,6 @@ from scipy.spatial import KDTree
 from itertools import product
 from .abstract_lattice import AbstractLattice
 
-_UNIT_CUBE_SIZE = 1.0
-_STRUT_NUMBER = 12
-_STRUT_HEIGHTS = 1.0
-_VERTICES = np.array(list(product([-_UNIT_CUBE_SIZE/2, _UNIT_CUBE_SIZE/2], repeat=3)))
-_STRUT_VERTEX_PAIRS = np.array([
-    [i, j] for i in range(len(_VERTICES)) 
-    for j in KDTree(_VERTICES).query_ball_point(_VERTICES[i], r=_STRUT_HEIGHTS) if i < j
-])
 
 class Cubic(AbstractLattice):
     """
@@ -19,17 +11,28 @@ class Cubic(AbstractLattice):
     """
 
     def __init__(self, *args, **kwargs) -> None:
-        super().__init__(*args, **kwargs, strut_number=_STRUT_NUMBER, strut_heights=_STRUT_HEIGHTS)
+        self.base_vertices = np.array(list(product([-self._UNIT_CUBE_SIZE/2, self._UNIT_CUBE_SIZE/2], repeat=3)))
+        self.strut_vertex_pairs = self._generate_vertex_pairs()
+        super().__init__(*args, **kwargs, strut_number=12, strut_heights=1.0)
 
     def _compute_vertices(self) -> npt.NDArray[np.float64]:
         """Compute the vertices of the cubic lattice."""
-        return self.center + self.cell_size * _VERTICES
+        return self.center + self.cell_size * self.base_vertices
 
     def _compute_strut_centers(self) -> npt.NDArray[np.float64]:
         """Compute the centers of the struts."""
-        return np.mean(self.vertices[_STRUT_VERTEX_PAIRS], axis=1)
+        return np.mean(self.vertices[self.strut_vertex_pairs(self._strut_heights)], axis=1)
 
     def _compute_strut_directions(self) -> npt.NDArray[np.float64]:
         """Compute the normalized direction vectors of the struts."""
-        vectors = np.diff(self.vertices[_STRUT_VERTEX_PAIRS], axis=1).squeeze()
+        vectors = np.diff(self.vertices[self.strut_vertex_pairs(self._strut_heights)], axis=1).squeeze()
         return vectors / np.linalg.norm(vectors, axis=1, keepdims=True)
+    
+    def _generate_vertex_pairs(self) -> npt.NDArray[int]:
+        """
+        Compute the strut vertex pairs.
+        """
+        return lambda strut_heights : np.array([
+            [i, j] for i in range(len(self.base_vertices)) 
+            for j in KDTree(self.base_vertices).query_ball_point(self.base_vertices[i], r=strut_heights) if i < j
+        ])
