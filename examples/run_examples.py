0d38ba1124943c30c4d40e366faf9e2e6f280c4a
Yves Chemisky <yves.chemisky@gmail.com>
2025-03-28 09:59:31 +0100

    Update run_examples.py

--- examples/run_examples.py
+++ examples/run_examples.py
@@ -134,7 +134,14 @@ if __name__ == "__main__":
 
     EXCLUDE = parser.parse_args().exclude
     if parser.parse_args().no_mmg:
-        EXCLUDE.extend(["Mesh/mmg", "Mesh/mmg-voro", "Mesh/remesh"])
+        EXCLUDE.extend(
+            [
+                "Mesh/mmg",
+                "Mesh/mmg-voro",
+                "Mesh/remesh",
+                "TPMS/gyroid/gyroid_step_remesh.py",
+            ]
+        )
     EXCLUDE.append("Fibers")
     EXCLUDE = [str(PARENT_DIR / exclude) for exclude in EXCLUDE]
 
