c2bc3ccd994c3526e6b72b841e66056cdbbb3e06
ylgrst <125015797+ylgrst@users.noreply.github.com>
2025-04-23 16:27:15 +0200

    Add AbstractLattice to strut_lattice init

--- microgen/shape/strut_lattice/__init__.py
+++ microgen/shape/strut_lattice/__init__.py
@@ -1,3 +1,4 @@
+from .abstract_lattice import AbstractLattice
 from .body_centered_cubic import BodyCenteredCubic
 from .cubic import Cubic
 from .cuboctahedron import Cuboctahedron
@@ -6,13 +7,14 @@ from .diamond import Diamond
 from .face_centered_cubic import FaceCenteredCubic
 from .octahedron import Octahedron
 from .octet_truss import OctetTruss
-from .truncated_cuboctahedron import TruncatedCuboctahedron
-from .truncated_cube import TruncatedCube
 from .rhombic_cuboctahedron import RhombicCuboctahedron
 from .rhombic_dodecahedron import RhombicDodecahedron
+from .truncated_cube import TruncatedCube
+from .truncated_cuboctahedron import TruncatedCuboctahedron
 from .truncated_octahedron import TruncatedOctahedron
 
 __all__ = [
+    "AbstractLattice",
     "BodyCenteredCubic",
     "Cubic",
     "Cuboctahedron",
